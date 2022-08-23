91a24d72c295badba65b0f2418c5827f5dbfa4b6
Jake Lee(이기원)/백엔드개발팀 <jake@ktown4u.com>
2022-08-23 10:03:26 +0900

    (test failed) add acceptance test cases.

---
+++ Chapter03/src/test/java/com/packt/modern/api/controllers/CartsControllerIntegrationTest.java
@@ -0,0 +1,54 @@
+package com.packt.modern.api.controllers;
+
+import io.restassured.module.mockmvc.RestAssuredMockMvc;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+
+import static io.restassured.module.mockmvc.RestAssuredMockMvc.given;
+import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.core.Is.is;
+
+class CartsControllerIntegrationTest {
+    private final String customerId = "123";
+
+    @BeforeEach
+    void setUp() {
+        RestAssuredMockMvc.standaloneSetup(new CartsController());
+    }
+
+    @Test
+    void getCartItemsByCustomerId_when_customer_exists() {
+        given()
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+
+                .when()
+                .get("/api/v1/carts/{customerId}", customerId)
+
+                .then()
+                .log().all()
+                .statusCode(200)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .assertThat()
+                .body("customerId", equalTo(customerId))
+                .body("items.size()", is(2));
+    }
+
+    @Test
+    void getCartItemsByCustomerId_when_customer_not_exists() {
+        given()
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+
+                .when()
+                .get("/api/v1/carts/{customerId}", "not-existing-customer")
+
+                .then()
+                .log().all()
+                .statusCode(404)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .assertThat()
+                .body(empty());
+    }
+
+}
