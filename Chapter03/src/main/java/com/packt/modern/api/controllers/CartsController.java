402930be2348bb340dff5489af859726f1193154
Jake Lee(이기원)/백엔드개발팀 <jake@ktown4u.com>
2022-08-23 10:03:04 +0900

    add CartApi implementation.

---
+++ Chapter03/src/main/java/com/packt/modern/api/controllers/CartsController.java
@@ -0,0 +1,8 @@
+package com.packt.modern.api.controllers;
+
+import com.packt.modern.api.CartApi;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+class CartsController implements CartApi {
+}
