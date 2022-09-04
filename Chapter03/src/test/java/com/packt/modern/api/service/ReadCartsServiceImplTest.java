cfcf7083fb9483552fa096d91555d8922cefbd1c
Jake Lee(이기원)/백엔드개발팀 <jake@ktown4u.com>
2022-09-04 19:09:00 +0900

    (test failed) CartRepositoryStub 추가.

--- Chapter03/src/test/java/com/packt/modern/api/service/ReadCartsServiceImplTest.java
+++ Chapter03/src/test/java/com/packt/modern/api/service/ReadCartsServiceImplTest.java
@@ -1,6 +1,10 @@
 package com.packt.modern.api.service;
 
+import com.packt.modern.api.domain.Cart;
+import com.packt.modern.api.domain.CartRepository;
 import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -8,7 +12,17 @@ class ReadCartsServiceImplTest {
 
     @Test
     void findById() {
-        final var sut = new ReadCartsServiceImpl(null);
+        final var sut = new ReadCartsServiceImpl(new CartRepositoryStub());
         assertThat(sut.findById("xxx")).isNotEmpty();
+    }
+
+    private class CartRepositoryStub implements CartRepository {
+        public Optional<Cart> findByCustomerId_will_returns = Optional.empty();
+
+        @Override
+        public Optional<Cart> findByCustomerId(final String customerId) {
+            return findByCustomerId_will_returns;
+        }
+
     }
 }
