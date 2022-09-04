5f59e5b2ab837f854c24de2aab7cd01531f8949c
Jake Lee(이기원)/백엔드개발팀 <jake@ktown4u.com>
2022-09-04 21:30:02 +0900

    (test failed) 테스트 케이스 추가.

--- Chapter03/src/test/java/com/packt/modern/api/domain/CartRepositoryIntegrationTest.java
+++ Chapter03/src/test/java/com/packt/modern/api/domain/CartRepositoryIntegrationTest.java
@@ -13,7 +13,17 @@ class CartRepositoryIntegrationTest {
     private CartRepository cartRepository;
 
     @Test
-    void findByCustomerId() {
+    void findByCustomerId_not_exists() {
         assertThat(cartRepository.findByCustomerId("xxx")).isEmpty();
+    }
+
+    @Test
+    void findByCustomerId_exists() {
+        final String customerId = "testing-customer-id";
+
+        final Cart cart = cartRepository.findByCustomerId(customerId).orElseThrow();
+
+        assertThat(cart.getCustomerId()).isEqualTo(customerId);
+        assertThat(cart.getItems()).hasSize(2);
     }
 }
